7095fef61eebe62333683d3850d21bf5fc4dee2a
tuftsBaxter <tuftsbaxter@gmail.com>
2017-07-14 13:35:12 -0400

    edited settings

---
+++ server2.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const request = require('request');
+
+// Storage object
+let storage = null;
+
+/**
+ * Get the property of a thing from the ThingWorx API.
+ * @param  {string}   thing    Name of the "thing" in the ThingWorx API.
+ * @param  {Function} callback
+ */
+function getProperty (thing, callback) {
+    request({
+        method: 'POST',
+        url: 'https://academic-ni.cloud.thingworx.com/Thingworx/Things/NIUtility/Services/GetPropertyInfo',
+        headers: {
+            "appkey": "9f1feea6-0659-4062-bef9-977ff92a75d6",
+        },
+        json: {
+            thingName: thing,
+            propertyName: ''
+        }
+    }, function (err, res, body) {
+        // Validate response
+        if (err) return callback(err);
+        if (typeof body.properties === 'undefined') return callback('404');
+        if (typeof body.properties.RandomNumber === 'undefined') return callback('404');
+        if (typeof body.properties.RandomNumber.value === 'undefined') return callback('404');
+
+        // Return the value
+        return callback(null, body.properties.RandomNumber.value);
+    });
+}
+
+setInterval(function () {
+    console.log(storage);
+    getProperty('ScratchPad_wcrooks', function (err, body) {
+        if (err) return;
+        storage = body;
+    });
+}, 1000);
+
+const server = http.createServer(function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.end(storage);
+});
+server.listen(8000);
